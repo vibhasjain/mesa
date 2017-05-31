a76c4fbde5bc426e018bb6a2d88760b235254aab
VIBES <VIBHAS111@GMAIL.COM>
2017-05-31 13:51:10 +0530

    Tried to unpublish district M menu - didnt work. this update fixes it

--- Mesa/AccessTokens.swift
+++ Mesa/AccessTokens.swift
@@ -11,4 +11,5 @@ import Foundation
 var spaces = ["bm180y4szy4w","kzo9qb155ouf"]
 var menus = ["64Ji1DtRrqeKKKUAW8CSIk","36wBG016IwICCq8w04C0Wq"]
 var tokens = ["ca94a6fc2f69767edb010d7f8634e13986c36e1c2e508edd6e7693ac9240bc3d",
-                "788d15800c51ca807cdcba6295c24b8c9e31139f31e2fd7ce87cf9319fff0489"]
+                "bafc1154590dfee0bc4d0e97d3f97f8552982595a6f4ae054861d30134969bc5"]
+
