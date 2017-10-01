dc24184359fb8d173f89710b250d7929f170448a
Rohit Khanwani <rohit@dhcp-hol-5405.redrover.cornell.edu>
2017-10-01 11:42:31 -0400

    Added intro screen everytime

--- Mesa/AppDelegate.swift
+++ Mesa/AppDelegate.swift
@@ -24,13 +24,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         BuddyBuildSDK.setup()
         
-        if UserDefaults.standard.bool(forKey: "onboardingHasAppeared") {
-            
-            self.storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
-            self.window?.rootViewController = self.storyboard?.instantiateViewController(withIdentifier: "HomeVC")
-
-            
-        }
+//        if UserDefaults.standard.bool(forKey: "onboardingHasAppeared") {
+//            
+//            self.storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+//            self.window?.rootViewController = self.storyboard?.instantiateViewController(withIdentifier: "HomeVC")
+//
+//            
+//        }
         
 //        Branch.getInstance().initSession(launchOptions: launchOptions) { params, error in
 //            // params are the deep linked params associated with the link that the user clicked -> was re-directed to this app
